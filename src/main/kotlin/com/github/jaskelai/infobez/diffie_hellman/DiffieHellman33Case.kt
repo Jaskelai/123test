efa06fdd0b7d1ab2f104c881301f0141e201eb59
jaskelai <jaskelais@gmail.com>
2020-04-19 13:47:42 +0300

    problem 33 solved

---
+++ src/main/kotlin/com/github/jaskelai/infobez/diffie_hellman/DiffieHellman33Case.kt
@@ -0,0 +1,48 @@
+package com.github.jaskelai.infobez.diffie_hellman
+
+import java.math.BigInteger
+import java.util.*
+
+class DiffieHellman33Case {
+
+    private companion object {
+        const val G = 2
+        const val P_HASH = "ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552bb9ed529077096966d670c354e4abc9804f1746c08ca237327ffffffffffffffff"
+    }
+
+    // g - начальное значение g
+    private val g: BigInteger = BigInteger(G.toString())
+
+    // p - начальное значение p
+    private val p = BigInteger(P_HASH, 16)
+
+    // a - секретный ключ
+    private val secretKey: BigInteger = nextRandomBigInt(p)
+
+    // А - публичный ключ
+    val publicKey: BigInteger = modExp(
+        base = g,
+        exponent = secretKey,
+        module = p
+    )
+
+    // s - сессионный ключ
+    fun getSessionKey(other: BigInteger): BigInteger = modExp(
+        base = other,
+        exponent = secretKey,
+        module = p
+    )
+
+    // Возведение в степень по модулю
+    private fun modExp(base: BigInteger, exponent: BigInteger, module: BigInteger) = base.modPow(exponent, module)
+
+    // Генерация a
+    private fun nextRandomBigInt(n: BigInteger): BigInteger {
+        val rand = Random()
+        var result = BigInteger(n.bitLength(), rand)
+        while (result >= n) {
+            result = BigInteger(n.bitLength(), rand)
+        }
+        return result
+    }
+}
---
+++ src/test/kotlin/DiffieHellman33CaseTest.kt
@@ -0,0 +1,20 @@
+import com.github.jaskelai.infobez.diffie_hellman.DiffieHellman33Case
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class DiffieHellman33CaseTest {
+
+    private val alice = DiffieHellman33Case()
+    private val bob = DiffieHellman33Case()
+
+    @Test
+    fun `test vectors assertion should be successful`() {
+
+        val sessionKeyAlice = alice.getSessionKey(bob.publicKey)
+        val sessionKeyBob = bob.getSessionKey(alice.publicKey)
+        println(sessionKeyAlice)
+        println(sessionKeyBob)
+
+        assertEquals(sessionKeyAlice, sessionKeyBob)
+    }
+}
